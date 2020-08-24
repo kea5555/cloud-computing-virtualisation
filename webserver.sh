7c7ab672a8b7c78ddb613f44f84d48f1173ba85e
rcole <racole1998@gmail.com>
2020-08-24 12:08:43 +1200

    Moved shell commands to separate files to keep code tidy

---
+++ webserver.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get install -y apache2 php libapache2-mod-php php-mysql
+    
+# Change VM's webserver's configuration to use shared folder.
+# (Look inside test-website.conf for specifics.)
+cp /vagrant/test-website.conf /etc/apache2/sites-available/
+# activate our website configuration ...
+a2ensite test-website
+# ... and disable the default website provided with Apache
+a2dissite 000-default
+# Reload the webserver configuration, to pick up our changes
+service apache2 reload
