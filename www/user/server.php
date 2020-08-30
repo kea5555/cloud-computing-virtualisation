b801eb9ea9f4541a13eec4e0c3da722767a5acdd
kea5555 <maim.montgomery@gmail.com>
2020-08-30 17:36:48 +1200

    Added a form to choose from differnt newsletters and so that the user gets directed to a page where they can see their choices.

---
+++ www/user/server.php
@@ -0,0 +1,22 @@
+
+<!-- TODO add connecton to the database so that there is a connection with the form to the database -->
+
+
+<!-- after someone has added their choices of newsleter -->
+<?php
+  $aDoor = $_POST['newsletter'];
+  if(empty($aDoor)) 
+  {
+    echo("You didn't select any newsletters to add.");
+  } 
+  else 
+  {
+    $N = count($aDoor);
+
+    echo("You selected $N newletter (s): ");
+    for($i=0; $i < $N; $i++)
+    {
+      echo($aDoor[$i] . " ");
+    }
+  }
+?>
